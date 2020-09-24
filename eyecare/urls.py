a785360268a25d3c533787d3b8ac6b1ac3238ea1
Sumit1673 <sumitvaise29@gmail.com>
2020-09-24 18:12:25 -0400

    intial commit

---
+++ eyecare/urls.py
@@ -0,0 +1,24 @@
+"""eyecare URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('eyecare', include('eyecare_app.urls')), if you have multiple apps it is
+    #  recommended to go with the specific app name. If you have just one then follow the next command
+    path('', include('eyecare_app.urls')),
+]
