9aa644773e23e9b063b94c38405857904707e19f
Sumit1673 <sumitvaise29@gmail.com>
2020-09-25 22:11:03 -0400

    color and navigation bar added

--- eyecare_app/urls.py
+++ eyecare_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name= 'app-home'),
-    path('about/', views.about, name= 'app-about')
-    path('workspace/', views.about, name= 'app-workspace')
+    path('about/', views.about, name= 'app-about'),
+    path('workspace/', views.workspace, name= 'app-workspace')
 ]
--- eyecare_app/views.py
+++ eyecare_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render 
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'eyecare_app/home.html') # returns httpresponse in background
 
 
 def about(request):
     return render(request, 'eyecare_app/about.html') 
+
 
 def workspace(request):
     return render(request, 'eyecare_app/workspace.html')
